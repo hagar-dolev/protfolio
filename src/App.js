df3254b5d9c3f6424c4b4a8ff595a30fcb8b5861
hagar-dolev <hagar.dolev@gmail.com>
2022-08-21 00:05:28 +0300

    making it work

--- src/App.js
+++ src/App.js
@@ -6,27 +6,28 @@ import Navbar from "./components/Navbar";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
 import Testimonials from "./pages/Testimonials";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Expirience from "./pages/Expirience";
 
 export default function App() {
-  return ( //Maybe main
-      <div className="text-neutral-600 bg-neutral-50 body-font">
-        <Navbar />
-          <Routes>
-              <Route path="/" element={<About/>}/>
-              <Route path="about" element={<About/>}/>
-              <Route path="projects" element={<Projects/>}/>
-              <Route path="skills" element={<Skills/>}/>
-              <Route path="Testimonials" element={<Testimonials/>}/>
-              <Route path="Contact" element={<Contact/>}/>
-              <Route path="Expirience" element={<Expirience/>}/>
-          </Routes>
-        {/*<About />*/}
-        {/*<Projects />*/}
-        {/*<Skills />*/}
-        {/*<Testimonials />*/}
-        {/*<Contact />*/}
-      </div>
+  return (
+    //Maybe main
+    <div className="text-neutral-600 bg-neutral-50 body-font">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/Testimonials" element={<Testimonials />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Expirience" element={<Expirience />} />
+      </Routes>
+      {/*<About />*/}
+      {/*<Projects />*/}
+      {/*<Skills />*/}
+      {/*<Testimonials />*/}
+      {/*<Contact />*/}
+    </div>
   );
 }
